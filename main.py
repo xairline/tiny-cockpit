f32214dc27594a5ce7c0f7f6d5ffc06933a0dfdc
Di Zou <di.zou1214@gmail.com>
2024-10-01 19:43:02 -0400

    test dual display

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ from layers.com2 import COM2
 
 def main():
 
-    display = Display()
+    display2 = Display(address=0x3D)
+    display = Display(address=0x3C)
     display.show("Search for XP ...", "", "")
+    display2.show("Search for XP ...", "", "")
     print()
     print("====================================")
     xp = XPlaneUdp()
@@ -16,27 +18,25 @@ def main():
         try:
             beacon = xp.FindIp()
             # single display
-            layers = {
-                "com1": COM1(xp, display),
-                "com2": COM2(xp, display),
-                # "nav1": NAV1(display),
-                # "nav2": NAV2(display),
-                # "adf1": ADF1(display),
-                # "adf2": ADF2(display),
-            }
-
-            # # double display
-            # display2 = Display(address=0x3d)
             # layers = {
             #     "com1": COM1(xp, display),
-            #     "com2": COM2(xp, display2),
+            #     "com2": COM2(xp, display),
             #     # "nav1": NAV1(display),
             #     # "nav2": NAV2(display),
             #     # "adf1": ADF1(display),
             #     # "adf2": ADF2(display),
             # }
+
+            # double display
+            layers = {
+                "com": [COM1(xp, display), COM2(xp, display2)],
+                # "nav1": NAV1(display),
+                # "nav2": NAV2(display),
+                # "adf1": ADF1(display),
+                # "adf2": ADF2(display),
+            }
             lastValuesHash = 0
-            active_layer = "com1"
+            active_layer = "com"
             print("====================================")
             print(f"X Plane IP: {beacon['IP']}")
             print(f"X Plane Port: {beacon['Port']}")
@@ -60,11 +60,16 @@ def main():
                             layers["com1"].show(values)
                         case "com2":
                             layers["com2"].show(values)
+                        case "com":
+                            layers["com"][0].show(values)
+                            layers["com"][1].show(values)
         except Exception as e:
             if isinstance(e, XPlaneTimeout):
                 display.error("Error", "XPlane Timeout", "Is Plane loaded?")
+                display2.error("Error", "XPlane Timeout", "Is Plane loaded?")
             elif isinstance(e, XPlaneIpNotFound):
                 display.error("Error", "XPlane not found", "Is XP Running?")
+                display2.error("Error", "XPlane Timeout", "Is Plane loaded?")
             else:
                 print(f"Exception type: {type(e)}")
             time.sleep(1)
