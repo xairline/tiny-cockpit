b7088c42181a5a748e1a77ce9ef4a1c7c161db91
Di Zou <di.zou1214@gmail.com>
2024-10-07 09:19:29 -0400

    local deploy

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,23 @@
-rsync -av --exclude '.venv/' --exclude '.venv/*' wireless/ di@cockpit.local:/home/di/cockpit
-rsync -av --exclude '.venv/' --exclude '.venv/*' hid/ di@hidpi.local:/home/di/cockpit
+deployment_type=$1
+ssh_string=$2
+
+if [ -z "$deployment_type" ]; then
+    deployment_type="wireless"
+    echo "No deployment type specified, defaulting to wireless"
+fi
+
+if [ -z "$ssh_string" ]; then
+    if [ "$deployment_type" == "wireless" ]; then
+        ssh_string="di@cockpit.local"
+    else
+        ssh_string="di@hidpi.local"
+    fi
+fi
+
+if [ "$deployment_type" == "wireless" ]; then
+    rsync -av --exclude '.venv/' --exclude '.venv/*' wireless/ $ssh_string:/home/di/cockpit
+elif [ "$deployment_type" == "hid" ]; then
+    rsync -av --exclude '.venv/' --exclude '.venv/*' hid/ $ssh_string:/home/di/cockpit
+else
+    echo "Invalid deployment type"
+fi
