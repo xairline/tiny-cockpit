d0bf547058953a37fc187f0805227652f0d4fe3c
Di Zou <di.zou1214@gmail.com>
2024-10-07 09:13:16 -0400

    refactor

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,2 @@
-rsync -av --exclude '.venv/' --exclude '.venv/*' . di@cockpit.local:/home/di/cockpit
+rsync -av --exclude '.venv/' --exclude '.venv/*' wireless/ di@cockpit.local:/home/di/cockpit
+rsync -av --exclude '.venv/' --exclude '.venv/*' hid/ di@hidpi.local:/home/di/cockpit
